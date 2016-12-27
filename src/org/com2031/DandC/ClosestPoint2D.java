d57d1b4296078dae28e82536d1a5274bae301427
Jack Simmonds <jacksimmonds0@gmail.com>
2016-12-27 13:35:17 +0100

    Implemented closest points 2D for even sized lists

--- src/org/com2031/DandC/ClosestPoint2D.java
+++ src/org/com2031/DandC/ClosestPoint2D.java
@@ -4,11 +4,84 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class ClosestPoint2D {
   
   public ClosestPoint2D() {
     super();
+  }
+  
+  public double minDistance(List<Point> currentPoints) {
+    double currentMin = 0;
+    double sepLine = 0;
+    int halfSize = 0;
+    List<Point> leftList = null;
+    List<Point> rightList = null;
+    
+    // base case
+    // when there are only two points in the list
+    if(currentPoints.size() == 2) {
+      return this.euclideanDistance(currentPoints.get(0), currentPoints.get(1));
+    }
+    
+    halfSize = currentPoints.size() / 2;
+    // define where the separation line is based on the current points list
+    sepLine = (currentPoints.get(halfSize).getX() + currentPoints.get(halfSize-1).getX()) / 2;
+    
+    leftList = new ArrayList<Point>(currentPoints.subList(0, halfSize));
+    rightList = new ArrayList<Point>(currentPoints.subList(halfSize, currentPoints.size()));
+
+    // divide and conquer - recursively call the method on both sides of the list of points
+    double leftDist = minDistance(leftList);
+    double rightDist = minDistance(rightList);
+
+    // find the minimum between the two halves of the list
+    currentMin = Math.min(leftDist, rightDist);
+    
+    // delete all points further than the min distance away from the seperation line
+    currentPoints = this.deletePointsFromLine(sepLine, currentMin, currentPoints);
+    // sort the remaining points by y co-ordinate
+    currentPoints = this.sortByYCoordinate(currentPoints);
+    
+    // all or all but 1 points deleted therefore return current min distance
+    if(currentPoints.size() == 0 || currentPoints.size() == 1) {
+      return currentMin;
+    }
+    else {
+      // loop through points and compare the adjacent points distances after sorted by Y
+      // return the min between that and the current min
+      return Math.min(currentMin, this.minFromList(currentPoints));
+    }
+    
+  }
+  
+  public double minFromList(List<Point> points) {
+    double min = 0;
+    for(int i = 0; i < points.size() - 1; i++) {
+      double dist = this.euclideanDistance(points.get(i), points.get(i + 1));
+      if(i == 0) {
+        min = dist;
+      }
+      else if(dist < min) {
+        min = dist;
+      }
+    }
+    return min;
+  }
+  
+  public List<Point> deletePointsFromLine(double sepLine, double distance, List<Point> points) { 
+    List<Point> tmp = points;
+    Iterator<Point> it = tmp.iterator();
+    
+    while(it.hasNext()) {
+      Point pt = it.next();
+      if(pt.getX() > sepLine + distance || pt.getX() < sepLine - distance) {
+        it.remove();
+      }
+    }
+    
+    return tmp;
   }
   
   
